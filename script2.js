36f60d034f2f82d41878c3ace8f5e18f8e263dd2
Gilson Carrara <102890937+gilcarrara@users.noreply.github.com>
2022-05-08 22:24:16 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,16 @@
+var numero;
+
+numero = prompt("Digite um número: ");
+
+if (numero > 0) {
+  alert("Número positivo");
+  }
+  else {  
+     if  (numero < 0) {
+     alert("Número negativo");
+     }
+     else {
+       alert("Número neutro");
+     }
+    }
+    
